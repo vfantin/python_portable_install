d636666caa885321f9a1cbae9c2e1682e85c5a02
Fantin <vfantin@cdpq.com>
2023-05-25 22:40:43 -0400

    Build

---
+++ install_drm.ps1
@@ -0,0 +1,4 @@
+wget https://raw.githubusercontent.com/vfantin/python_portable_install/master/drm_20230525223825.zip -o drm_20230525223825.zip
+Expand-Archive drm_20230525223825.zip -DestinationPath .
+#cd drm
+#.\report.ps1 .\config\yesterday_drm_report.config
--- install_python.ps1
+++ install_python.ps1
@@ -1,10 +1,9 @@
-#wget https://raw.githubusercontent.com/vfantin/python_portable_install/master/python-3.11.0-amd64.zip -o python-3.11.0-amd64.zip
-#Expand-Archive python-3.11.0-amd64.zip -DestinationPath .
-#rm python-3.11.0-amd64.zip
+wget https://raw.githubusercontent.com/vfantin/python_portable_install/master/python-3.11.0-amd64.zip -o python-3.11.0-amd64.zip
+Expand-Archive python-3.11.0-amd64.zip -DestinationPath .
+rm python-3.11.0-amd64.zip
 .\python-3.11.0-amd64\python.exe -m venv venv
 .\venv\Scripts\Activate.ps1
 python --version
 pip --version
 python.exe -m pip install --upgrade pip
-pip install -r .\requirements.txt
 deactivate
