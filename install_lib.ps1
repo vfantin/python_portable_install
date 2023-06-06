85fb5724884e305fa0a62b92e20325dab1f95879
Fantin <vfantin@cdpq.com>
2023-06-06 18:29:36 -0400

    Build a standalone disrib

--- install_lib.ps1
+++ install_lib.ps1
@@ -1,7 +1,7 @@
 wget https://raw.githubusercontent.com/vfantin/python_portable_install/master/requirements.txt -o requirements.txt
 wget https://raw.githubusercontent.com/vfantin/python_portable_install/master/cdpq_report-0.0.1.tar.gz -o cdpq_report-0.0.1.tar.gz
 wget https://raw.githubusercontent.com/vfantin/python_portable_install/master/cdpq_utils-0.0.7.tar.gz -o cdpq_utils-0.0.7.tar.gz
-wget https://raw.githubusercontent.com/vfantin/python_portable_install/master/pyodbc-4.0.34-cp311-cp311-win_amd64.whl
+wget https://raw.githubusercontent.com/vfantin/python_portable_install/master/pyodbc-4.0.34-cp311-cp311-win_amd64.whl -o pyodbc-4.0.34-cp311-cp311-win_amd64.whl
 .\venv\Scripts\Activate.ps1
 pip install -r .\requirements.txt
 pip freeze
